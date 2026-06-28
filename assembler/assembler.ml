9bcadc7476c3a87c69825b87991cb4a8cce3c436
ktm06 <mikikippei@gmail.com>
2026-06-28 00:28:31 -0400

    implement base file splitting logic

--- assembler/assembler.ml
+++ assembler/assembler.ml
@@ -1,4 +1,10 @@
 open Str
 
+let contents = In_channel.with_open_text "instr.txt" In_channel.input_all
+
 let testinstruction = "add rd, rs1, rs2"
-let linesplitter Str.split(Str.regexp "[ ,]+") 
+
+let filesplitter input = String.split_on_char '\n' input
+let linesplitter input = List.map ( fun x -> Str.split(Str.regexp "[ ,]+") x) input
+
+let parsed = contents |> filesplitter |> linesplitter
